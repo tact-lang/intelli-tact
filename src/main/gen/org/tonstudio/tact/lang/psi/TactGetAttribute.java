9397d1e4282be2e4a1ca9b96123425085f72a132
i582 <51853996+i582@users.noreply.github.com>
2025-04-30 22:20:57 +0400

    fix: add missed files

---
+++ src/main/gen/org/tonstudio/tact/lang/psi/TactGetAttribute.java
@@ -0,0 +1,19 @@
+// This is a generated file. Not intended for manual editing.
+package org.tonstudio.tact.lang.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface TactGetAttribute extends TactCompositeElement {
+
+  @Nullable
+  TactExpression getExpression();
+
+  @Nullable
+  PsiElement getLparen();
+
+  @Nullable
+  PsiElement getRparen();
+
+}
