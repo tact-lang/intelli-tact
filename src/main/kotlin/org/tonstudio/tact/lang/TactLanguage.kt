d34801c1018606791a4a8d2950039d7a2130b854
i582 <51853996+i582@users.noreply.github.com>
2025-05-05 01:29:47 +0400

    feat: add MIME types to TactLanguage definition

--- src/main/kotlin/org/tonstudio/tact/lang/TactLanguage.kt
+++ src/main/kotlin/org/tonstudio/tact/lang/TactLanguage.kt
@@ -2,7 +2,10 @@ package org.tonstudio.tact.lang
 
 import com.intellij.lang.Language
 
-object TactLanguage : Language("tact") {
+object TactLanguage : Language(
+    "tact",
+    "text/tact", "text/x-tact", "application/x-tact",
+) {
     override fun isCaseSensitive() = true
     override fun getDisplayName() = "Tact"
     private fun readResolve(): Any = TactLanguage
