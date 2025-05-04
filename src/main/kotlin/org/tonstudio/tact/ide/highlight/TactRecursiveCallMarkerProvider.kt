afb7c2483778e239a7490837a1fb0c9bf364ee72
i582 <51853996+i582@users.noreply.github.com>
2025-05-05 01:01:44 +0400

    feat: add gutter icon for recursive calls
    Fixes #70

---
+++ src/main/kotlin/org/tonstudio/tact/ide/highlight/TactRecursiveCallMarkerProvider.kt
@@ -0,0 +1,50 @@
+package org.tonstudio.tact.ide.highlight
+
+import com.intellij.codeInsight.daemon.LineMarkerInfo
+import com.intellij.codeInsight.daemon.LineMarkerProvider
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.editor.markup.GutterIconRenderer
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.parentOfType
+import com.intellij.util.FunctionUtil
+import org.tonstudio.tact.lang.psi.TactCallExpr
+import org.tonstudio.tact.lang.psi.TactFunctionOrMethodDeclaration
+import org.tonstudio.tact.lang.psi.TactReferenceExpression
+
+class TactRecursiveCallMarkerProvider : LineMarkerProvider {
+    override fun getLineMarkerInfo(element: PsiElement) = null
+
+    override fun collectSlowLineMarkers(elements: List<PsiElement>, result: MutableCollection<in LineMarkerInfo<*>?>) {
+        val lines = mutableSetOf<Int>()
+        for (element in elements) {
+            if (element !is TactCallExpr) continue
+
+            val resolve = (element.expression as? TactReferenceExpression)?.resolve() as? TactFunctionOrMethodDeclaration ?: continue
+
+            if (isRecursiveCall(element, resolve)) {
+                val document = PsiDocumentManager.getInstance(element.project).getDocument(element.containingFile) ?: continue
+                val lineNumber = document.getLineNumber(element.textOffset)
+                if (!lines.contains(lineNumber)) {
+                    result.add(RecursiveMethodCallMarkerInfo(element.identifier ?: element))
+                }
+
+                lines.add(lineNumber)
+            }
+        }
+    }
+
+    private class RecursiveMethodCallMarkerInfo(methodCall: PsiElement) : LineMarkerInfo<PsiElement?>(
+        methodCall,
+        methodCall.textRange,
+        AllIcons.Gutter.RecursiveMethod,
+        FunctionUtil.constant("Recursive call"),
+        null,
+        GutterIconRenderer.Alignment.RIGHT,
+        { "Recursive call" }
+    )
+
+    private fun isRecursiveCall(element: PsiElement, function: TactFunctionOrMethodDeclaration): Boolean {
+        return element.parentOfType<TactFunctionOrMethodDeclaration>() == function
+    }
+}
