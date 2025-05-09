175c120a374a580dbaf4fee46e79246e43b02397
Petr Makhnev <51853996+i582@users.noreply.github.com>
2025-05-09 15:28:29 +0400

    feat(actions): add action to build and disasm contract (#117)

---
+++ src/main/kotlin/org/tonstudio/tact/ide/actions/ShowAssemblyForContractAction.kt
@@ -0,0 +1,50 @@
+package org.tonstudio.tact.ide.actions
+
+import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.fileTypes.PlainTextLanguage
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiFileFactory
+import com.intellij.psi.util.parentOfType
+import org.tonstudio.tact.asm.TonBocDisassembler
+import org.tonstudio.tact.ide.build.openFileInRightTab
+import org.tonstudio.tact.ide.build.runBackground
+import org.tonstudio.tact.ide.build.single.builcContract
+import org.tonstudio.tact.lang.psi.TactCompositeElement
+import org.tonstudio.tact.lang.psi.TactContractDeclaration
+import org.tonstudio.tact.lang.psi.TactFile
+import kotlin.io.path.Path
+
+class ShowAssemblyForContractAction : SelectionBasedPsiElementAction<TactCompositeElement>(TactCompositeElement::class.java) {
+    override fun actionPerformed(element: TactCompositeElement) {
+        val project = element.project
+        val contract = element.parentOfType<TactContractDeclaration>() ?: return
+        val contractName = contract.name
+        val contrainingFile = contract.containingFile as? TactFile ?: return
+        val virtualFile = contrainingFile.virtualFile ?: return
+        val filepath = virtualFile.path
+
+        val handler = builcContract(project, filepath) {
+            val filename = Path(filepath).fileName.toString()
+            val artifactName = "${filename}_$contractName.code.boc"
+            val artifactPath = virtualFile.parent?.findChild("output")?.findChild(artifactName) ?: return@builcContract
+
+            val disasm = TonBocDisassembler(project)
+            val disassembledText = disasm.disassemble(artifactPath.path) ?: "Cannot disassemble '$artifactPath'."
+
+            val disasmFile = runReadAction { createFile(project, disassembledText) }
+            val disasmVirtualFile = disasmFile?.virtualFile ?: return@builcContract
+
+            openFileInRightTab(project, disasmVirtualFile) { _, _ -> }
+        }
+
+        handler.startNotify()
+
+        runBackground(project, "Building Tact contract '$contractName'...") {
+            handler.waitFor(30_000)
+        }
+    }
+
+    private fun createFile(project: Project, disassembledText: String): PsiFile? =
+        PsiFileFactory.getInstance(project).createFileFromText("disasm.fif", PlainTextLanguage.INSTANCE, disassembledText, true, true)
+}
--- src/main/kotlin/org/tonstudio/tact/ide/build/single/TactBuildConfigurationRunState.kt
+++ src/main/kotlin/org/tonstudio/tact/ide/build/single/TactBuildConfigurationRunState.kt
@@ -11,6 +11,7 @@ import com.intellij.execution.process.ProcessListener
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.runners.ProgramRunner
 import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.findPsiFile
 import org.tonstudio.tact.configurations.TactConfigurationUtil
@@ -31,72 +32,82 @@ class TactBuildConfigurationRunState(
         val state = object : CommandLineState(env) {
             override fun startProcess(): ProcessHandler {
                 val project = env.project
-
-                val root = project.toolchainSettings.toolchain().rootDir()
-                if (root == null) {
-                    throw Error("Can't build: ${TactConfigurationUtil.TOOLCHAIN_NOT_SETUP}")
-                }
-
-                val tactCompiler = root.findChild("bin")?.findChild("tact.js")
-                if (tactCompiler == null) {
-                    throw Error("Can't build: Cannot find Tact compiler executable")
-                }
-
-                val (contractPath, projectRoot) = tryRelativizePath(project, conf.fileName)
-
-                val commandLine = GeneralCommandLine()
-                    .withExePath(tactCompiler.path)
-                    .withWorkDirectory(projectRoot)
-                    .withCharset(Charsets.UTF_8)
-                    .withParameters(contractPath)
-                    .withRedirectErrorStream(true)
-
-                val handler = TactProcessHandler(commandLine)
-
-                handler.addProcessListener(object : ProcessListener {
-                    override fun processTerminated(event: ProcessEvent) {
-                        moveBuildArtifacts()
-                    }
-
-                    private fun moveBuildArtifacts() {
-                        val root = Path(conf.fileName).parent.toString()
-                        val filename = Path(conf.fileName).fileName.toString()
-
-                        val extensions = "(abi|code\\.boc|fc|fif|md|pkg|ts)"
-                        val contracts = runReadAction { definedContracts(conf.fileName) }
-                        val contractsRe = if (contracts.isNotEmpty()) contracts.joinToString("|", "(", ")") else ".*?"
-                        val regex = Regex("${filename}_$contractsRe\\.$extensions")
-
-                        val srcDir = Path(root)
-                        val outDir = srcDir.resolve("output")
-                        Files.createDirectories(outDir)
-
-                        Files.list(srcDir).use { stream ->
-                            stream.filter { Files.isRegularFile(it) }
-                                .filter { regex.matches(it.fileName.toString()) }
-                                .forEach { file ->
-                                    Files.move(
-                                        file,
-                                        outDir.resolve(file.fileName),
-                                        StandardCopyOption.REPLACE_EXISTING
-                                    )
-                                }
-                        }
-                    }
-                })
-
-
-                return handler
-            }
-
-            private fun definedContracts(filepath: String): List<String> {
-                val file = VfsUtil.findFile(Path(filepath), true) ?: return emptyList()
-                val psiFile = file.findPsiFile(env.project) as? TactFile ?: return emptyList()
-                val contracts = psiFile.getContracts()
-                return contracts.map { it.name }
+                return builcContract(project, conf.fileName)
             }
         }
 
         return state.execute(executor, runner)
     }
+}
+
+fun builcContract(
+    project: Project,
+    filepath: String,
+    onReady: () -> Unit = {},
+): TactProcessHandler {
+    val root = project.toolchainSettings.toolchain().rootDir()
+    if (root == null) {
+        throw Error("Can't build: ${TactConfigurationUtil.TOOLCHAIN_NOT_SETUP}")
+    }
+
+    val tactCompiler = root.findChild("bin")?.findChild("tact.js")
+    if (tactCompiler == null) {
+        throw Error("Can't build: Cannot find Tact compiler executable")
+    }
+
+    val (contractPath, projectRoot) = tryRelativizePath(project, filepath)
+
+    val commandLine = GeneralCommandLine()
+        .withExePath(tactCompiler.path)
+        .withWorkDirectory(projectRoot)
+        .withCharset(Charsets.UTF_8)
+        .withParameters(contractPath)
+        .withRedirectErrorStream(true)
+
+    val handler = TactProcessHandler(commandLine)
+
+    handler.addProcessListener(object : ProcessListener {
+        override fun processTerminated(event: ProcessEvent) {
+            moveBuildArtifacts()
+        }
+
+        private fun moveBuildArtifacts() {
+            val root = Path(filepath).parent.toString()
+            val filename = Path(filepath).fileName.toString()
+
+            val extensions = "(abi|code\\.boc|fc|fif|md|pkg|ts)"
+            val contracts = runReadAction { definedContracts(project, filepath) }
+            val contractsRe = if (contracts.isNotEmpty()) contracts.joinToString("|", "(", ")") else ".*?"
+            val regex = Regex("${filename}_$contractsRe\\.$extensions")
+
+            val srcDir = Path(root)
+            val outDir = srcDir.resolve("output")
+            Files.createDirectories(outDir)
+
+            Files.list(srcDir).use { stream ->
+                stream.filter { Files.isRegularFile(it) }
+                    .filter { regex.matches(it.fileName.toString()) }
+                    .forEach { file ->
+                        Files.move(
+                            file,
+                            outDir.resolve(file.fileName),
+                            StandardCopyOption.REPLACE_EXISTING
+                        )
+                    }
+            }
+
+            VfsUtil.markDirtyAndRefresh(false, true, true, srcDir.toFile())
+            onReady()
+        }
+    })
+
+
+    return handler
+}
+
+private fun definedContracts(project: Project, filepath: String): List<String> {
+    val file = VfsUtil.findFile(Path(filepath), true) ?: return emptyList()
+    val psiFile = file.findPsiFile(project) as? TactFile ?: return emptyList()
+    val contracts = psiFile.getContracts()
+    return contracts.map { it.name }
 }
